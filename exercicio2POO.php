745a0f0de57621a6e0323c36d34c6dd4683f92c0
kavs1346 <130669072+kavs1346@users.noreply.github.com>
2023-10-20 10:39:17 -0300

    ( ರ Ĺ̯ ರೃ )

---
+++ exercicio2POO.php
@@ -0,0 +1,315 @@
+<?php
+    include("pessoa.class.php");
+
+    $p1 = new pessoa();
+
+        $p1->nome = "Kauan Silva Vieira";
+        $p1->altura = 1.69;
+        $p1->nascimento = "2006-09-02";
+        $p1->hobbie = "Jogar";
+
+
+    echo "Nome: ".$p1->nome."<br/>
+        Altura: ".$p1->altura."<br/>
+    Nascimento: ".$p1->nascimento."<br/>
+        Hobbie: ".$p1->hobbie."<br/><br/><hr><br/>";
+
+    $p2 = new pessoa();
+
+        $p2->nome = "Kauã Lira";
+        $p2->altura = 1.63;
+        $p2->nascimento = "2007-01-05";
+        $p2->hobbie = "Tocar Guitarra";
+
+    $p3 = new pessoa();
+
+        $p3->nome = "Thell Zardi";
+        $p3->altura = 1.65;
+        $p3->nascimento = "2006-10-31";
+        $p3->hobbie = "Jogar";
+
+    $p4 = new pessoa();
+
+        $p4->nome = "Pedro Araújo";
+        $p4->altura = 1.68;
+        $p4->nascimento = "2006-09-24";
+        $p4->hobbie = "Jogar videogame e andar de bike";
+    
+    $p5 = new pessoa();
+
+        $p5->nome = "Henrique Tavares";
+        $p5->altura = 1.63;
+        $p5->nascimento = "2006-07-26";
+        $p5->hobbie = "Jogar/Ouvir Música";
+
+    $p6 = new pessoa();
+
+        $p6->nome = "José Luiz Oliveira Costa";
+        $p6->altura = 1.71;
+        $p6->nascimento = "2006-09-15";
+        $p6->hobbie = "Assistir/Critica de filme";
+
+    $p7 = new pessoa();
+
+        $p7->nome = "Ryan Silveira";
+        $p7->altura = 1.72;
+        $p7->nascimento = "2006-09-02";
+        $p7->hobbie = "Jogar VideoGame";
+
+    $p8 = new pessoa();
+
+        $p8->nome = "Pedro Santos";
+        $p8->altura = 1.80;
+        $p8->nascimento = "2006-10-24";
+        $p8->hobbie = "Tocar Violão";
+
+    $p9 = new pessoa();
+
+        $p9->nome = "Guilherme Carvalho";
+        $p9->altura = 1.75;
+        $p9->nascimento = "2007-06-09";
+        $p9->hobbie = "Jogar VideoGame";
+
+    $p10 = new pessoa();
+
+        $p10->nome = "Júlia Mogi";
+        $p10->altura = 1.70;
+        $p10->nascimento = "2006-11-08";
+        $p10->hobbie = "Cozinhar";
+
+    $p11 = new pessoa();
+
+        $p11->nome = "Laura Perez";
+        $p11->altura = 1.70;
+        $p11->nascimento = "2007-08-03";
+        $p11->hobbie = "Ler";
+
+    $p12 = new pessoa();
+
+        $p12->nome = "Natally Reis";
+        $p12->altura = 1.60;
+        $p12->nascimento = "2007-01-07";
+        $p12->hobbie = "Ouvir Música";
+
+    $p13 = new pessoa();
+
+        $p13->nome = "Samuel Americo";
+        $p13->altura = 1.85;
+        $p13->nascimento = "2007-01-226";
+        $p13->hobbie = "Tocar Piano";
+
+    $p14 = new pessoa();
+
+        $p14->nome = "Beatriz Baccaro";
+        $p14->altura = 1.60;
+        $p14->nascimento = "2006-08-02";
+        $p14->hobbie = "Esporte";
+
+    $p15 = new pessoa();
+
+        $p15->nome = "Luis Gustavo Magela";
+        $p15->altura = 1.77;
+        $p15->nascimento = "2007-03-06";
+        $p15->hobbie = "Dormir e Jogar VideoGame";
+
+    $p16 = new pessoa();
+
+        $p16->nome = "Leticia Gabrielle";
+        $p16->altura = 1.65;
+        $p16->nascimento = "2006-09-30";
+        $p16->hobbie = "Ouvir música e assistir série";
+
+    $p17 = new pessoa();
+
+        $p17->nome = "Camilly Vitória de Oliveira";
+        $p17->altura = 1.60;
+        $p17->nascimento = "2007-01-22";
+        $p17->hobbie = "Ouvir música/fazer aula de jazz";
+
+    $p18 = new pessoa();
+
+        $p18->nome = "Ana Clara Maciel";
+        $p18->altura = 1.50;
+        $p18->nascimento = "2007-01-19";
+        $p18->hobbie = "Ler e ouvir música";
+
+    $p19 = new pessoa();
+
+        $p19->nome = "Ana Zinatto";
+        $p19->altura = 1.60;
+        $p19->nascimento = "2007-11-03";
+        $p19->hobbie = "Treinar/Ouvir música";
+    
+    $p20 = new pessoa();
+
+        $p20->nome = "Gustavo Otaviano";
+        $p20->altura = 1.85;
+        $p20->nascimento = "2007-03-04";
+        $p20->hobbie = "Esportes";
+
+    $p21 = new pessoa();
+
+        $p21->nome = "Gabriel Credendio";
+        $p21->altura = 1.70;
+        $p21->nascimento = "2006-07-28";
+        $p21->hobbie = "Jogar VideoGame";
+
+    $p22 = new pessoa();
+
+        $p22->nome = "Raí Franca";
+        $p22->altura = 1.82;
+        $p22->nascimento = "2006-09-12";
+        $p22->hobbie = "Esporte, ouvir música, jogos eletrônicos";
+
+    $p23 = new pessoa();
+
+        $p23->nome = "Ana Clara Ferreira";
+        $p23->altura = 1.55;
+        $p23->nascimento = "2007-03-21";
+        $p23->hobbie = "Pintar";
+
+    $p24 = new pessoa();
+
+        $p24->nome = "Gabriel Alves Santos";
+        $p24->altura = 1.84;
+        $p24->nascimento = "2006-08-03";
+        $p24->hobbie = "Jogar videogame, praticar esportes";
+
+    $p25 = new pessoa();
+
+        $p25->nome = "Gabriel Diaz";
+        $p25->altura = 1.74;
+        $p25->nascimento = "2007-03-21";
+        $p25->hobbie = "Jogar";
+
+    $p26 = new pessoa();
+
+        $p26->nome = "Beatriz Mayumi";
+        $p26->altura = 1.65;
+        $p26->nascimento = "2007-03-03";
+        $p26->hobbie = "Ler livros, jogar videogame e ouvir música";
+
+    $p27 = new pessoa();
+
+        $p27->nome = "Duda";
+        $p27->altura = 1.65;
+        $p27->nascimento = "2007-04-02";
+        $p27->hobbie = "Ver série e jogar vôlei";
+
+    $p28 = new pessoa();
+
+        $p28->nome = "Giovana Lupo";
+        $p28->altura = 1.62;
+        $p28->nascimento = "2007-05-14";
+        $p28->hobbie = "Pintar";
+
+    $p29 = new pessoa();
+
+        $p29->nome = "Bruno Alves";
+        $p29->altura = 1.78;
+        $p29->nascimento = "2007-05-28";
+        $p29->hobbie = "Tocar instrumentos";
+
+    $p30 = new pessoa();
+
+        $p30->nome = "Felipe Wilbert";
+        $p30->altura = 1.78;
+        $p30->nascimento = "2007-12-06";
+        $p30->hobbie = "Academia";
+
+    $p31 = new pessoa();
+
+        $p31->nome = "Miguel Romero";
+        $p31->altura = 1.70;
+        $p31->nascimento = "2007-04-14";
+        $p31->hobbie = "Jogar futebol";
+
+    $p32 = new pessoa();
+
+        $p32->nome = "Henrique Nagata";
+        $p32->altura = 1.58;
+        $p32->nascimento = "2007-10-25";
+        $p32->hobbie = "Andar de bicicleta";
+
+    $p33 = new pessoa();
+
+        $p33->nome = "Pedro Grechi";
+        $p33->altura = 1.69;
+        $p33->nascimento = "2007-03-23";
+        $p33->hobbie = "Jogar/Desenhar";
+
+
+
+    // echo "Nome: ".$p3->nome."<br/>
+    //     Altura: ".$p3->altura."<br/>
+    // Nascimento: ".$p3->nascimento."<br/>
+    //     Hobbie: ".$p3->hobbie."<br/><br/><hr><br/>";
+
+    echo "Abaixo um exemplo pratico do uso de metodos : <br/><br/>";
+    $p1->MostrarPessoa();
+    echo "<br/><br/>";
+    $p2->MostrarPessoa();
+    echo "<br/><br/>";
+    $p3->MostrarPessoa();
+    echo "<br/><br/>";
+    $p4->MostrarPessoa();
+    echo "<br/><br/>";
+    $p5->MostrarPessoa();
+    echo "<br/><br/>";
+    $p6->MostrarPessoa();
+    echo "<br/><br/>";
+    $p7->MostrarPessoa();
+    echo "<br/><br/>";
+    $p8->MostrarPessoa();
+    echo "<br/><br/>";
+    $p9->MostrarPessoa();
+    echo "<br/><br/>";
+    $p10->MostrarPessoa();
+    echo "<br/><br/>";
+    $p11->MostrarPessoa();
+    echo "<br/><br/>";
+    $p12->MostrarPessoa();
+    echo "<br/><br/>";
+    $p13->MostrarPessoa();
+    echo "<br/><br/>";
+    $p14->MostrarPessoa();
+    echo "<br/><br/>";
+    $p15->MostrarPessoa();
+    echo "<br/><br/>";
+    $p16->MostrarPessoa();
+    echo "<br/><br/>";
+    $p17->MostrarPessoa();
+    echo "<br/><br/>";
+    $p18->MostrarPessoa();
+    echo "<br/><br/>";
+    $p19->MostrarPessoa();
+    echo "<br/><br/>";
+    $p20->MostrarPessoa();
+    echo "<br/><br/>";
+    $p21->MostrarPessoa();
+    echo "<br/><br/>";
+    $p22->MostrarPessoa();
+    echo "<br/><br/>";
+    $p23->MostrarPessoa();
+    echo "<br/><br/>";
+    $p24->MostrarPessoa();
+    echo "<br/><br/>";
+    $p25->MostrarPessoa();
+    echo "<br/><br/>";
+    $p26->MostrarPessoa();
+    echo "<br/><br/>";
+    $p27->MostrarPessoa();
+    echo "<br/><br/>";
+    $p28->MostrarPessoa();
+    echo "<br/><br/>";
+    $p29->MostrarPessoa();
+    echo "<br/><br/>";
+    $p30->MostrarPessoa();
+    echo "<br/><br/>";
+    $p31->MostrarPessoa();
+    echo "<br/><br/>";
+    $p32->MostrarPessoa();
+    echo "<br/><br/>";
+    $p33->MostrarPessoa();
+
+?>
